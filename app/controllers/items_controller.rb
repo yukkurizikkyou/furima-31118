7778d3315e1ec872076c913e0a26202063de1ef1
yukkurizikkyou <ym12270413@gmail.com>
2020-11-13 20:05:03 +0900

    test

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except:[:index]
   before_action :set_item, only: [:show, :edit, :update]
   before_action :move_to_index, only: [:edit]
+  
   def index
    @items = Item.all.order(created_at: :DESC)
   end
