c419b9b449c6bafd91716d5838b99ed8b5626aea
yukkurizikkyou <ym12270413@gmail.com>
2020-11-13 16:52:25 +0900

    商品編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,6 +21,19 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
   private
   def item_params
     params.require(:item).permit(:name, :image, :explanation, :category_id, :day_id , :condition_id, :delivery_fee_id, :area_id, :price ).merge(user_id: current_user.id)
